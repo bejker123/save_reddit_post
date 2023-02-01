eede790ea78c7ab24988d5c9dd51d510823a6f68
bejker123 <1piotrkucharski@gmail.com>
2023-02-01 10:58:57 +0100

    !fix: getting 'more' elements;now hopefully getting all elements feat: calculating more elements eta, precentage; time formatting; add printing status docs: add comments describing functionality;incremented version refactor: remove some debug code; remove some unwraps known bug: reddit doesn't count deleted comments in their num_comments metric, but this program does, which creates a discrepancy!

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use cli::*;
 
 mod tests;
 
-use std::{io::Write, time::SystemTime};
+use std::{io::Write, time::SystemTime, process::exit};
 
 async fn request(url : String) -> reqwest::Response{
     let client = reqwest::Client::new();
@@ -18,39 +18,110 @@ async fn request(url : String) -> reqwest::Response{
     }
 }
 
+//Convert time in seconds to a more readable format
+// Xh Ymin Zs
+fn convert_time(t : f64) -> String{
+    if t == 0.0{
+        String::new()
+    } 
+    else if t < 60.0{
+        format!("{t:.2}s")
+    }else if t < 3600.0{
+        ((t/60.0) as i32).to_string() + "min " + &convert_time(t % 60.0)
+    }else{
+        ((t/3600.0) as i32).to_string() + "h " + &convert_time(t % 3600.0)
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    print!("Initialising: ");
     let args: Vec<String> = std::env::args().collect();
     let start = SystemTime::now();
     let cli = CLI::new(args);
 
+    println!("success.");
+
+    print!("Requesting content from {}\nStatus: ",cli.url);
     let res = request(cli.url).await;
 
     let data = match res.text().await {
-        Ok(o) => o,
-        _ => todo!(), //TODO: add restarting
+        Ok(o) => {
+            println!("success.");
+            o
+        },
+        _ => {
+            println!("fail.");
+            exit(1);
+        }, 
     };
+
     println!(
         "Downloaded content in {} ms",
         start.elapsed().unwrap().as_millis()
     );
-    let json_data = json::parse(&data).unwrap();
 
+    print!("Parsing to JSON: ");
+
+    let json_data = match json::parse(&data){
+        Ok(o)=>{
+            println!("success.");
+            o
+        },
+        _ => {
+            println!("fail.");
+            exit(1);
+        }, 
+    };
+
+
+    print!("Writing to JSON file: ");
     let start = SystemTime::now();
-    std::fs::write("raw.json.tmp", json_data.pretty(1)).unwrap();
+    match std::fs::write("raw.json.tmp", json_data.pretty(1)){
+        Ok(_)=>{
+            print!("success ");
+        },
+        Err(e) => panic!("fail.\nError: {e}"), 
+    }
     println!(
-        "Written to file in {} ms",
+        "(in {} ms)",
         start.elapsed().unwrap().as_millis()
     );
+
+    print!("Parsing to elements: ");
+
     let start = std::time::SystemTime::now();
 
     let mut elements = Element::init(&json_data);
 
+    if !elements.is_empty(){
+        println!("success.");
+    }else{
+        println!("fail.");
+    }
+    
+
+    let more_start = std::time::SystemTime::now();
+
+    let mut last_passed = 0u128;
+
+    let mut last_line_length = 0usize;
+
+    
+
+    //'more' elements
     unsafe {
+        if MORE_ELEMENTS_COUNT > 0{
+            println!("Getting 'more' elements:");
+        }
+        let base_elements = ELEMENTS_COUNT;
+
+        //Get more elements from the 'more' listing
         for more_element in &MORE_ELEMENTS {
+            //build the url
             let url = cli.base_url.clone() + more_element + ".json";
             let res = request(url).await;
-
+            
             let data = match res.text().await {
                 Ok(o) => o,
                 _ => todo!(), //TODO: add restarting
@@ -58,11 +129,62 @@ async fn main() {
 
             let json_data = json::parse(&data).unwrap();
 
+            //Parse json data to elements
             let mut e = Element::init(&json_data);
-            ELEMENTS_COUNT -= 1;
+            //ELEMENTS_COUNT -= 1; //TODO: remove this?
+            
+            //Don't count elements from base file.
+            let more_elem_c = ELEMENTS_COUNT - base_elements;
+
+            //calculate % of progress as a 64bit float 64
+            let precent = (more_elem_c as f64)/((NUM_COMMENTS - base_elements) as f64)*100f64;
+
+            //get time passed since start of getting 'more' elements 
+            let passed = std::time::SystemTime::now().duration_since(more_start).unwrap().as_millis();
+
+            //calculate delta between the last request
+            let delta = passed - last_passed;
+            last_passed = passed;
+
+            //Get estimated time
+            let eta = (NUM_COMMENTS - base_elements) as f64 / (more_elem_c as f64 / passed as f64);
+
+            //Format the line to be printed
+            let mut line = format!("{more_elem_c} / {} {:.2}% passed: {} delta: {}ms ETA: {}",
+            MORE_ELEMENTS.len(),precent,
+            convert_time(passed as f64/1000f64),
+            delta,
+            convert_time((eta - passed as f64)/1000f64));
+            
+            let line_length = line.len();
+            
+            //Make sure there is no residual chars from last line
+            if line_length < last_line_length{
+                line += &" ".repeat(last_line_length-line_length);
+            }
+            last_line_length = line_length;
+
+            //Print the line and flush stdout
+            //If you don;t flush stdout not every line will be printed,
+            //Because print! doesn't flush as oppose to println!
+            print!("\r{line}");
+            std::io::stdout().flush();
+
+            //
             if e.len() < 2{
+                elements.append(&mut e[0].children);
+                // let mut file = std::fs::OpenOptions::new()
+                // .append(true)
+                // .create(true)
+                // .open("discarded.tmp")
+                // .unwrap();
+                // for x in &e{
+                //     file.write_fmt(format_args!("{x:?}\n"));
+                // }
                 continue;
             }
+
+            //Recursively check every element, and if the first element matches appedn to it
             fn app(x : &mut Vec<Element>,y : &mut Vec<Element>){
                 for element in &mut *y{
                     if x.is_empty(){
@@ -70,7 +192,8 @@ async fn main() {
                     }
                     if *element == x[0]{
                         x.remove(0);
-                        print!("Append ");
+                        unsafe{ELEMENTS_COUNT -= 1;}
+                       // print!("Append ");
                         element.children.append(x);
                         break;
                     }
@@ -81,9 +204,7 @@ async fn main() {
             }
 
             app(&mut e,&mut elements);
-
-            
-            print!("Elements: {} delta: {}              \r",ELEMENTS_COUNT,start.elapsed().unwrap().as_millis());
+           // print!("Elements: {} delta: {}              \r",ELEMENTS_COUNT,start.elapsed().unwrap().as_millis());
         }
         
         if ELEMENTS_COUNT == 0 {
@@ -91,26 +212,40 @@ async fn main() {
         }
     }
 
+    println!("");
+    //Set the default output to stdout
     let mut output: Box<dyn Write> = Box::new(std::io::stdout());
 
+    //If user specified saving to a file.
+    //Change the output to the file.
     if cli.save_to_file {
         output = Box::new(
             std::fs::OpenOptions::new()
                 .create(true)
                 .write(true)
                 .truncate(true)
-                .open(cli.save_path)
+                .open(cli.save_path.clone())
                 .unwrap(),
         );
+        print!("Writing to {}: ",cli.save_path);
+    }else{
+        print!("Writing to stdout");
     }
 
+    //Write every element to the output.
+    //For formatting see element.rs:
+    //                   impl std::fmt::Debug for Element
     for elem in elements {
         match output.write_fmt(format_args!("{elem:?}")) {
-            Ok(()) => {}
-            Err(e) => panic!("Failed to write to output!\nError: {e}"),
+            //Ignore success
+            Ok(()) => {},
+            //If failed to write to output, panic and finish execution.
+            Err(e) => panic!("fail.\nError: {e}"), 
         }
     }
-    
+    println!("success");
+
+    //Print last bit of debug data
     unsafe {
         println!("MORE_ELEMENTS_COUNT: {MORE_ELEMENTS_COUNT}\nMORE_ELEMENTS.len(): {}\n{}",MORE_ELEMENTS.len(),MORE_ELEMENTS_COUNT == MORE_ELEMENTS.len());
         print!(
@@ -119,5 +254,5 @@ async fn main() {
             if ELEMENTS_COUNT == 1 { "" } else { "s" }
         );
     };
-    println!(", in {} ms", start.elapsed().unwrap().as_millis());
+    println!(", in {}", convert_time(start.elapsed().unwrap().as_millis() as f64 /1000f64));
 }
