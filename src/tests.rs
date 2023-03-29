9c0a3ca052cda8f69f40543e18a4ca89b59452ec
bejker123 <1piotrkucharski@gmail.com>
2023-03-29 21:00:48 +0200

    fix: tests

--- src/tests.rs
+++ src/tests.rs
@@ -52,33 +52,33 @@ fn test_cli() {
     //This test is incomplete!
 
     CLI::new(&[st("test-bin"), st("-h")]);
-    let cli1 = CLI::new(&[st("test-bin"), st("https://test-url.com/")]);
+    let cli1 = CLI::new(&[st("test-bin"), st("https://reddit.com/r/asd")]);
     assert_eq!(
         (cli1.url, cli1.base_url),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        (st("https://reddit.com/r/asd.json"), st("https://reddit.com/r/asd/"))
     );
-    let cli2 = CLI::new(&[st("test-bin"), st("https://test-url.com/")]);
+    let cli2 = CLI::new(&[st("test-bin"), st("https://reddit.com/r/")]);
     assert_eq!(
         (cli2.url, cli2.base_url),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        (st("https://reddit.com/r.json"), st("https://reddit.com/r/"))
     );
     assert_eq!(
         CLI::new(&[
             st("test-bin"),
             st("-s"),
             st("test-path.txt"),
-            st("https://test-url.com/")
+            st("https://reddit.com/r/asd")
         ]),
         CLI {
-            url: st("https://test-url.com.json"),
-            base_url: st("https://test-url.com/"),
+            url: st("https://reddit.com/r/asd.json"),
+            base_url: st("https://reddit.com/r/asd/"),
             save_to_file: true,
             save_path: st("test-path.txt"),
             max_comments: USIZE_MAX,
             filter: CLI_ELEMENT_FILTER_DEF,
             sort_style: CLI_ELEMENT_SORT_DEF,
             save_tmp_files: false,
-            verbosity: cli::Verbosity::Low,
+            verbosity: cli::Verbosity::Moderate,
             req_more_elements: true,
             delete_tmp: false,
             print_timestamps: false,
@@ -90,35 +90,35 @@ fn test_cli() {
             st("-o"),
             st("-s"),
             st("test-path.txt"),
-            st("https://test-url.com/")
+            st("https://reddit.com/r/")
         ]),
         CLI {
-            url: st("https://test-url.com.json"),
-            base_url: st("https://test-url.com/"),
+            url: st("https://reddit.com/r.json"),
+            base_url: st("https://reddit.com/r/"),
             save_to_file: false,
             save_path: st("test-path.txt"),
             max_comments: USIZE_MAX,
             filter: CLI_ELEMENT_FILTER_DEF,
             sort_style: CLI_ELEMENT_SORT_DEF,
             save_tmp_files: false,
-            verbosity: cli::Verbosity::Low,
+            verbosity: cli::Verbosity::Moderate,
             req_more_elements: true,
             delete_tmp: false,
             print_timestamps: false,
         }
     );
     assert_eq!(
-        CLI::new(&[st("test-bin"), st("-o"), st("https://test-url.com/")]),
+        CLI::new(&[st("test-bin"), st("-o"), st("https://reddit.com/r/")]),
         CLI {
-            url: st("https://test-url.com.json"),
-            base_url: st("https://test-url.com/"),
+            url: st("https://reddit.com/r.json"),
+            base_url: st("https://reddit.com/r/"),
             save_to_file: false,
             save_path: st("output.txt"),
             max_comments: USIZE_MAX,
             filter: CLI_ELEMENT_FILTER_DEF,
             sort_style: CLI_ELEMENT_SORT_DEF,
             save_tmp_files: false,
-            verbosity: cli::Verbosity::Low,
+            verbosity: cli::Verbosity::Moderate,
             req_more_elements: true,
             delete_tmp: false,
             print_timestamps: false,
@@ -130,18 +130,18 @@ fn test_cli() {
             st("test-bin"),
             st("-o"),
             st("--save-tmp"),
-            st("https://test-url.com/")
+            st("https://reddit.com/r/")
         ]),
         CLI {
-            url: st("https://test-url.com.json"),
-            base_url: st("https://test-url.com/"),
+            url: st("https://reddit.com/r.json"),
+            base_url: st("https://reddit.com/r/"),
             save_to_file: false,
             save_path: st("output.txt"),
             max_comments: USIZE_MAX,
             filter: CLI_ELEMENT_FILTER_DEF,
             sort_style: CLI_ELEMENT_SORT_DEF,
             save_tmp_files: true,
-            verbosity: cli::Verbosity::Low,
+            verbosity: cli::Verbosity::Moderate,
             req_more_elements: true,
             delete_tmp: false,
             print_timestamps: false,
@@ -152,40 +152,40 @@ fn test_cli() {
 #[test]
 fn test_cli_parse_url() {
     assert_eq!(
-        CLI::parse_url(st("https://test-url.com/")),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        CLI::parse_url(st("https://reddit.com/r/asd")),
+        (st("https://reddit.com/r/asd.json"), st("https://reddit.com/r/asd/"))
     );
     assert_eq!(
-        CLI::parse_url(st("https://test-url.com/asd/?foo&bar:443")),
+        CLI::parse_url(st("https://reddit.com/r/asd/?foo&bar:443")),
         (
-            st("https://test-url.com/asd.json"),
-            st("https://test-url.com/asd/")
+            st("https://reddit.com/r/asd.json"),
+            st("https://reddit.com/r/asd/")
         )
     );
     assert_eq!(
-        CLI::parse_url(st("https://test-url.com/asd:443")),
+        CLI::parse_url(st("https://reddit.com/r/asd:443")),
         (
-            st("https://test-url.com/asd.json"),
-            st("https://test-url.com/asd/")
+            st("https://reddit.com/r/asd.json"),
+            st("https://reddit.com/r/asd/")
         )
     );
     assert_eq!(
-        CLI::parse_url(st("https://test-url.com:443")),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        CLI::parse_url(st("https://reddit.com/r/:443")),
+        (st("https://reddit.com/r.json"), st("https://reddit.com/r/"))
     );
     assert_eq!(
-        CLI::parse_url(st("https://test-url.com\n\n\n ")),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        CLI::parse_url(st("https://reddit.com/r/\n\n\n ")),
+        (st("https://reddit.com/r.json"), st("https://reddit.com/r/"))
     );
     assert_eq!(
-        CLI::parse_url(st("\nhttps://test-url.com\n\n\n ")),
-        (st("https://test-url.com.json"), st("https://test-url.com/"))
+        CLI::parse_url(st("\nhttps://reddit.com/r/\n\n\n ")),
+        (st("https://reddit.com/r.json"), st("https://reddit.com/r/"))
     );
     assert_eq!(
-        CLI::parse_url(st("\nhttps://test-url.com\n\n\n /foo")),
+        CLI::parse_url(st("\nhttps://reddit.com/r//foo\n")),
         (
-            st("https://test-url.com/foo.json"),
-            st("https://test-url.com/foo/")
+            st("https://reddit.com/r//foo.json"),
+            st("https://reddit.com/r//foo/")
         )
     );
 }
